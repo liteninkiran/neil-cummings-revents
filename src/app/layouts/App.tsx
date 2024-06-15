6465ffbd06d9e0ca8e293cb658e6a86aedecb3b1
Kiran Anand <liteninkiran@hotmail.com>
2024-06-15 21:16:54 +0100

    016 - Semantic UI introduction

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from 'semantic-ui-react';
+
 function App() {
     return (
-        <h1>
-            Welcome to Revents
-        </h1>
+        <div>
+            <h1>
+                Welcome to Revents
+            </h1>
+            <button className='ui icon red button'><i className='user icon'></i> CSS Button</button>
+            <Button icon='smile' content='React Button' color='green' label='Label' />
+        </div>
     );
 }
 
