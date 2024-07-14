e43fd0e843829c8dc3b777cc0e4534277b6d180c
Kiran Anand <liteninkiran@hotmail.com>
2024-07-14 12:34:58 +0100

    073 - Shaping the firestore data

--- src/features/events/eventSlice.ts
+++ src/features/events/eventSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppEvent } from '../../app/types/event';
+import { Timestamp } from 'firebase/firestore';
 
 type State = {
     events: AppEvent[]
@@ -16,7 +17,12 @@ export const eventSlice = createSlice({
         createEvent: (state, action) => { state.events.push(action.payload); },
         updateEvent: (state, action) => { state.events[state.events.findIndex(evt => evt.id === action.payload.id)] = action.payload; },
         deleteEvent: (state, action) => { state.events.splice(state.events.findIndex(evt => evt.id === action.payload), 1) },
-        setMyEvents: (state, action) => { state.events = action.payload; },
+        setMyEvents: {
+            reducer: (state, action: PayloadAction<AppEvent[]>) => { state.events = action.payload; },
+            prepare: (events) =>
+                ({ payload: events.map((e: any) =>
+                    ({ ...e, date: (e.date as Timestamp).toDate().toISOString() })) }),
+        },
     },
 });
 
