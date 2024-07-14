7340905972b129c3bd6775d62b23ba46bb477456
Kiran Anand <liteninkiran@hotmail.com>
2024-07-14 16:28:00 +0100

    078 - Deleting data from Firestore

--- src/features/events/dashboard/EventListItem.tsx
+++ src/features/events/dashboard/EventListItem.tsx
@@ -1,16 +1,29 @@
 import { Button, Icon, Item, ItemGroup, List, Segment, SegmentGroup } from 'semantic-ui-react';
-import EventListAttendee from './EventListAttendee';
 import { AppEvent } from '../../../app/types/event';
 import { Link } from 'react-router-dom';
-import { useAppDispatch } from '../../../app/store/store';
-import { deleteEvent } from '../eventSlice';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../../../app/config/firebase';
+import EventListAttendee from './EventListAttendee';
 
 type Props = {
     event: AppEvent;
 }
 
 export default function EventListItem({ event }: Props) {
-    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(false);
+    const removeEvent = async () => {
+        setLoading(true);
+        try {
+            await deleteDoc(doc(db, 'events', event.id));
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <SegmentGroup>
             <Segment>
@@ -42,7 +55,7 @@ export default function EventListItem({ event }: Props) {
 
             <Segment clearing>
                 <span>{event.description}</span>
-                <Button color='red' floated='right' content='Delete' onClick={() => dispatch(deleteEvent(event.id))} />
+                <Button loading={loading} color='red' floated='right' content='Delete' onClick={removeEvent} />
                 <Button color='teal' floated='right' content='View' as={Link} to={`/events/${event.id}`} />
             </Segment>
         </SegmentGroup>
--- src/features/events/eventSlice.ts
+++ src/features/events/eventSlice.ts
@@ -3,7 +3,7 @@ import { AppEvent } from '../../app/types/event';
 import { Timestamp } from 'firebase/firestore';
 
 type State = {
-    events: AppEvent[]
+    events: AppEvent[];
 }
 
 const initialState: State = {
@@ -14,18 +14,13 @@ export const eventSlice = createSlice({
     name: 'events',
     initialState,
     reducers: {
-        createEvent: (state, action) => { state.events.push(action.payload); },
-        updateEvent: (state, action) => { state.events[state.events.findIndex(evt => evt.id === action.payload.id)] = action.payload; },
-        deleteEvent: (state, action) => { state.events.splice(state.events.findIndex(evt => evt.id === action.payload), 1) },
         setMyEvents: {
             reducer: (state, action: PayloadAction<AppEvent[]>) => { state.events = action.payload; },
-            prepare: (events: AppEvent[]) => {
-
-                return ({ payload: events.map((e: any) =>
-                    ({ ...e, date: (e.date as Timestamp).toDate().toISOString() })) })
-            },
+            prepare: (events: AppEvent[]) =>
+                ({ payload: events.map((e: any) =>
+                    ({ ...e, date: (e.date as Timestamp).toDate().toISOString() })) }),
         },
     },
 });
 
-export const {createEvent, updateEvent, deleteEvent, setMyEvents} = eventSlice.actions;
+export const {setMyEvents} = eventSlice.actions;
