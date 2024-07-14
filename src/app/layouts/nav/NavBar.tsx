9c7d37183a712c968e99f2292fed912ee87f1d8a
Kiran Anand <liteninkiran@hotmail.com>
2024-07-14 11:47:50 +0100

    071 - Adding data to firestore

--- src/app/layouts/nav/NavBar.tsx
+++ src/app/layouts/nav/NavBar.tsx
@@ -3,9 +3,18 @@ import { Button, Container, Menu, MenuItem } from 'semantic-ui-react';
 import SignedOutButtons from './SignedOutButtons';
 import SignedInMenu from './SignedInMenu';
 import { useAppSelector } from '../../store/store';
+import { sampleData } from '../../api/sampleData';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../../config/firebase';
 
 export default function NavBar() {
-    const { authenticated: auth } = useAppSelector(state => state.auth)
+    const { authenticated: auth } = useAppSelector(state => state.auth);
+    const seedData = () => {
+        sampleData.forEach(async event => {
+            const { id, ...rest } = event;
+            await setDoc(doc(db, 'events', id), { ...rest });
+        });
+    }
     return (
         <Menu inverted={true} fixed='top'>
             <Container>
@@ -24,10 +33,18 @@ export default function NavBar() {
                         positive={true}
                         inverted={true}
                         content='Create Event'
-                         as={NavLink}
-                         to='/createEvent'
+                        as={NavLink}
+                        to='/createEvent'
                     />
                 </MenuItem>
+
+                {
+                    import.meta.env.DEV && (
+                        <MenuItem>
+                            <Button inverted={true} color='teal' content='Seed Data' onClick={seedData} />
+                        </MenuItem>
+                    )
+                }
 
                 {
                     auth ? (
